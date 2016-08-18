6f952ed97fbaa596f9a7004f8ef956fb97cbafa6
DKomarovskij <dkomarovski18@gmail.com>
2016-08-19 02:28:19 +0300

    refactoring like clear code

--- 3.laba.z_№_45.cpp
+++ 3.laba.z_№_45.cpp
@@ -9,51 +9,83 @@ using namespace std;
 const int N = 1000;
 char *strk = new char[N];
 
+void showtask();
+
+void fin(char *ps, int length);
+
+void watchstr(char *ps);
+
+void strget (char *ps);
+
+void numssum (int len, int tmplen, int &sum, char * maxslovo, int &maxsumma, char *ps );
+
+void maketask1 (char *ps);
+
+void PrintMenu();
+
+int menu ();
+
+int chooseMenu();
+
 /*45. (*) В строке найти сумму цифр, содержащихся во всех словах и слово с максимальной суммой цифр.*/
-void showtask()
-{
+
+int main(){
+    setlocale(LC_ALL,"Russia");
+    while (1){
+        menu();
+    }
+}
+
+int menu (){
+        PrintMenu();
+        chooseMenu();
+}
+void PrintMenu(){
+        cout << "Введите 1, чтобы просмотреть текст задания." << endl;//+
+		cout << "Введите 2, чтобы получить строку из файла." << endl;//+
+		cout << "Введите 3, чтобы просмотреть строку." << endl;
+		cout << "Введите 4, чтобы самостоятельно ввести строку." << endl;
+		cout << "Введите 5, чтобы выполнить задание." << endl;
+		cout << "Введите 6, чтобы хотите выйти из программы." << endl;
+}
+
+int chooseMenu(){
+        char *pstr = strk;
+        int status = 0;
+        cin >> status;
+        switch (status)
+        {
+            case 1:{showtask();      break;}
+            case 2:{fin(pstr, N);    break;}
+            case 3:{watchstr(pstr);  break;}
+            case 4:{strget(pstr);    break;}
+            case 5:{maketask1(pstr); break;}
+            case 6:{return 1;}
+            default: cout << "Введите число заново" << endl;break;
+        }
+
+}
+
+void showtask() {
     cout << "В строке найти сумму цифр, содержащихся во всех словах и слово с максимальной суммой цифр."<< endl;
 }
-void fin(char *ps, int length)
-{
 
+void fin(char *ps, int length){
     ifstream fin("file.txt");
     fin.getline(ps, length);
 }
-void watchstr(char *ps)
-{
+
+void watchstr(char *ps){
     cout << ps << endl;
 }
-void strget (char *ps)
-{
+
+void strget (char *ps){
     cout << "Введите вашу строку" << endl;
     cin.get();
     cin.getline(strk, N);
 }
-void numssum (int len, int tmplen, int &sum, char * maxslovo, int &maxsumma, char *ps )
-{
-    int peresum = 0;
-    char* slovo = new char [N];
-    strncpy(slovo, ps+tmplen, len);
-    *(slovo+len)='\0';
-    char * symbol;
-    for (int i = 1; i < 10; i++ )
-    {
-        char * tmpslovo = slovo;
-        while (1)
-        {
-            char *cifra ;
-            char symbola = i+48;
-            cifra = strchr(tmpslovo,symbola);
-            if (cifra!=NULL) {peresum = peresum + i;tmpslovo = cifra+1;}
-            else {break;}
-        }
-    }
-    sum = sum + peresum;
-    if (peresum > maxsumma){strncpy(maxslovo, ps+tmplen,len); maxsumma = peresum; }
-}
-void maketask1 (char *ps)
-{
+
+void maketask1 (char *ps){
     char* maxslovo = new char[255];
     int maxsumma = 0;
     char sym1[] = ",.!?:;-+()*?%№=_&^$#/|\[]{}<>'~`@ ";
@@ -70,55 +102,23 @@ void maketask1 (char *ps)
 
 }
 
-int main()
-{
-    setlocale(LC_ALL,"Russia");
-    char *pstr = strk;
-    int status = 0;
-    while (1)
-    {
-        cout << "Введите 1, чтобы просмотреть текст задания." << endl;//+
-		cout << "Введите 2, чтобы получить строку из файла." << endl;//+
-		cout << "Введите 3, чтобы просмотреть строку." << endl;
-		cout << "Введите 4, чтобы самостоятельно ввести строку." << endl;
-		cout << "Введите 5, чтобы выполнить задание." << endl;
-		cout << "Введите 6, чтобы хотите выйти из программы." << endl;
-        cin >> status;
-        switch (status)
-        {
-            case 1://+
-            {
-                showtask();
-                break;
-            }
-            case 2:
-            {
-                fin(pstr, N);
-                break;
-            }
-           case 3:
-            {
-                watchstr(pstr);
-                break;
-            }
-            case 4:
-            {
-                strget(pstr);
-                break;
-            }
-            case 5:
-            {
-               maketask1(pstr);
-               break;
-            }
-            case 6:
-            {
-                return 1;
-
-            }
-            default: cout << "Введите число заново" << endl;
-            break;
+void numssum (int len, int tmplen, int &sum, char * maxslovo, int &maxsumma, char *ps ){
+    int peresum = 0;
+    char* slovo = new char [N];
+    strncpy(slovo, ps+tmplen, len);
+    *(slovo+len)='\0';
+    char * symbol;
+    for (int i = 1; i < 10; i++ ){
+        char * tmpslovo = slovo;
+        while (1){
+            char *cifra ;
+            char symbola = i+48;
+            cifra = strchr(tmpslovo,symbola);
+            if (cifra!=NULL) {peresum = peresum + i;tmpslovo = cifra+1;}
+            else {break;}
         }
     }
-
+    sum = sum + peresum;
+    if (peresum > maxsumma){strncpy(maxslovo, ps+tmplen,len); maxsumma = peresum; }
 }
+
